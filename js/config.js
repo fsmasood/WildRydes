4c6a0db18a59315c6cf1b4b9ade9213b901aff99
Farhan <fsmasood@mun.ca>
2023-07-30 16:39:27 -0230

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ca-central-1_UBWJTQNQP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '506pfd0updshvibtv5f37mlisr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ca-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://8llho6x6x4.execute-api.ca-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
